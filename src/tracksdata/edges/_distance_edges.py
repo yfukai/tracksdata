65f0ede5b1126ccd35a2caee2a71bf139ff06e76
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2025-07-08 16:30:10 -0700

    fixing when adding attribute to existing key

--- src/tracksdata/edges/_distance_edges.py
+++ src/tracksdata/edges/_distance_edges.py
@@ -108,7 +108,8 @@ class DistanceEdges(BaseEdgesOperator):
         """
         Initialize the edge attributes for the graph.
         """
-        graph.add_edge_attr_key(self.output_key, default_value=-99999.0)
+        if self.output_key not in graph.edge_attr_keys:
+            graph.add_edge_attr_key(self.output_key, default_value=-99999.0)
 
     def _add_edges_per_time(
         self,
