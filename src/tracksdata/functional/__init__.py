cb4770a3a4cb5c02805d07107df6046654d1cc26
Jordão Bragantini <jordao.bragantini@czbiohub.org>
2025-11-04 08:03:33 -0800

    Apply tiled functionality for tiled processing of graphs (#183)
    * adding apply tiled and tests
    
    * fixing np.arange iteration
    
    * adding tile object
    
    * improving import
    
    * renaming
    
    * adding default value to agg_func
    
    * renaming 'tile' and 'overlap' to 'tile_shape' and 'overlap_shape'
    
    * adding different scale and data types testing
    
    * fixing typing
    
    * improving testing
    
    * using nextafter for eps
    
    * additional fix
    
    * fix testing
    
    * testing spacing
    
    * Revert "testing spacing"
    
    This reverts commit 6988d911f7635a95f3ccad1ca3152e2a9e3d1754.

--- src/tracksdata/functional/__init__.py
+++ src/tracksdata/functional/__init__.py
@@ -1,6 +1,7 @@
 """Functional utilities for graph operations."""
 
+from tracksdata.functional._apply import TilingScheme, apply_tiled
 from tracksdata.functional._edges import join_node_attrs_to_edges
 from tracksdata.functional._napari import rx_digraph_to_napari_dict, to_napari_format
 
-__all__ = ["join_node_attrs_to_edges", "rx_digraph_to_napari_dict", "to_napari_format"]
+__all__ = ["TilingScheme", "apply_tiled", "join_node_attrs_to_edges", "rx_digraph_to_napari_dict", "to_napari_format"]
