2ad4fea176a02a017af7c697a0bd7feb39db89d6
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2025-12-09 09:29:53 -0800

    reusing existing caching mechanism

--- src/tracksdata/graph/filters/_base_filter.py
+++ src/tracksdata/graph/filters/_base_filter.py
@@ -1,11 +1,7 @@
 import abc
-import functools
-from collections.abc import Callable
-from typing import TYPE_CHECKING, Any, TypeVar
+from typing import TYPE_CHECKING, Any
 
 import polars as pl
-
-from tracksdata.utils._logging import LOG
 
 if TYPE_CHECKING:
     from tracksdata.graph._graph_view import GraphView
@@ -57,57 +53,3 @@ class BaseFilter(abc.ABC):
         """
         Get a subgraph of the graph resulting from the filter.
         """
-
-
-def _make_hashable(obj: Any) -> Any:
-    if isinstance(obj, tuple | list):
-        return tuple(_make_hashable(o) for o in obj)
-    elif isinstance(obj, dict):
-        return tuple(sorted((_make_hashable(k), _make_hashable(v)) for k, v in obj.items()))
-    elif isinstance(obj, set):
-        return frozenset(_make_hashable(o) for o in obj)
-    elif hasattr(obj, "__dict__"):
-        return _make_hashable(vars(obj))  # for custom objects
-    elif hasattr(obj, "__hash__") and obj.__hash__:
-        return obj
-    else:
-        return repr(obj)  # fallback for anything else
-
-
-F = TypeVar("F", bound=Callable[..., Any])
-
-
-def cache_method(func: F) -> F:
-    """
-    Cache the result of a method.
-
-    Parameters
-    ----------
-    func : Callable[..., Any]
-        The method to cache.
-
-    Returns
-    -------
-    Callable[..., Any]
-        The wrapped method.
-    """
-
-    @functools.wraps(func)
-    def _wrapper(self: BaseFilter, *args: Any, **kwargs: Any) -> Any:
-        try:
-            key = (
-                func.__name__,
-                _make_hashable(args),
-                _make_hashable(kwargs),
-            )
-        except Exception as e:
-            LOG.warning(f"_{func.__name__} failed to hash {args} and {kwargs}:\n{e}")
-            return func(self, *args, **kwargs)
-
-        if key in self._cache:
-            return self._cache[key]
-        result = func(self, *args, **kwargs)
-        self._cache[key] = result
-        return result
-
-    return _wrapper
