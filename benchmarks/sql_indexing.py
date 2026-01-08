368185b2a58a94aa4e723a80b4f45ba4e6b22fb3
Yohsuke Fukai <ysk@yfukai.net>
2026-01-08 10:15:47 +0900

    formatted

--- benchmarks/sql_indexing.py
+++ benchmarks/sql_indexing.py
@@ -7,17 +7,19 @@ else:
 
 NODE_COUNT = 1_000_000
 
+
 class SQLIndexingBenchmark:
     def setup(self) -> None:
-        graph : td.graph.SQLGraph = BACKENDS["SQLGraphDisk"]()
+        graph: td.graph.SQLGraph = BACKENDS["SQLGraphDisk"]()
         graph.add_node_attr_key("attr1", 0)
-        graph.bulk_add_nodes([{td.DEFAULT_ATTR_KEYS.T: i, "attr1": i%100} for i in range(NODE_COUNT)])
+        graph.bulk_add_nodes([{td.DEFAULT_ATTR_KEYS.T: i, "attr1": i % 100} for i in range(NODE_COUNT)])
         if hasattr(graph, "ensure_node_attr_index"):
             graph.ensure_node_attr_index("attr1")
         self.graph = graph
 
     def time_sql_index_search(self) -> None:
         self.graph.filter(td.NodeAttr("attr1") == 0).subgraph()
+
 
 if __name__ == "__main__":
     import cProfile
