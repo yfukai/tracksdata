dde23bd8c8668e48f2e75660867c389fad8e2308
Jordão Bragantini <jordao.bragantini@czbiohub.org>
2026-01-22 13:27:24 -0800

    Adding `neighbors_per_frame` option to `DistanceEdges` (#216)
    * adding neighbors_per_frame option
    
    * fixing range for non-int values
    
    * updated docs
    
    * fixing test
    
    * improving testing

--- src/tracksdata/edges/_test/test_distance_edges.py
+++ src/tracksdata/edges/_test/test_distance_edges.py
@@ -312,3 +312,166 @@ def test_distance_edges_multiprocessing_isolation() -> None:
     """Test that multiprocessing options don't affect subsequent tests."""
     # Verify default n_workers is 1
     assert get_options().n_workers == 1
+
+
+def test_distance_edges_neighbors_per_frame_false() -> None:
+    """Test neighbors_per_frame=False behavior with delta_t=2."""
+    graph = RustWorkXGraph()
+
+    # Register attribute keys
+    graph.add_node_attr_key("x", 0.0)
+    graph.add_node_attr_key("y", 0.0)
+
+    # Add nodes at t=0, t=1, t=2
+    # At t=0: two nodes close to origin
+    graph.add_node({DEFAULT_ATTR_KEYS.T: 0, "x": 0.0, "y": 0.0})
+    graph.add_node({DEFAULT_ATTR_KEYS.T: 0, "x": 0.5, "y": 0.0})
+
+    # At t=1: two nodes slightly further
+    node_1a = graph.add_node({DEFAULT_ATTR_KEYS.T: 1, "x": 1.0, "y": 0.0})
+    node_1b = graph.add_node({DEFAULT_ATTR_KEYS.T: 1, "x": 1.5, "y": 0.0})
+
+    # At t=2: one node at (2, 0)
+    node_2 = graph.add_node({DEFAULT_ATTR_KEYS.T: 2, "x": 2.0, "y": 0.0})
+
+    # With neighbors_per_frame=False, delta_t=2, n_neighbors=2:
+    # Node at t=2 should connect to 2 closest nodes from combined t=0 and t=1
+    # (which should be the t=1 nodes since they're closer)
+    operator = DistanceEdges(
+        distance_threshold=5.0,
+        n_neighbors=2,
+        delta_t=2,
+        neighbors_per_frame=False,
+    )
+
+    operator.add_edges(graph)
+
+    edges_df = graph.edge_attrs()
+    t2_edges = edges_df.filter(edges_df[DEFAULT_ATTR_KEYS.EDGE_TARGET] == node_2)
+
+    # Should have exactly 2 edges (n_neighbors=2 total)
+    assert len(t2_edges) == 2
+
+    # Both edges should be from t=1 nodes (closest to t=2 node)
+    sources = set(t2_edges[DEFAULT_ATTR_KEYS.EDGE_SOURCE].to_list())
+    assert sources == {node_1a, node_1b}
+
+
+def test_distance_edges_neighbors_per_frame_true() -> None:
+    """Test neighbors_per_frame=True behavior with delta_t=2."""
+    graph = RustWorkXGraph()
+
+    # Register attribute keys
+    graph.add_node_attr_key("x", 0.0)
+    graph.add_node_attr_key("y", 0.0)
+
+    # Add nodes at t=0, t=1, t=2
+    # At t=0: two nodes close to origin
+    node_0a = graph.add_node({DEFAULT_ATTR_KEYS.T: 0, "x": 0.0, "y": 0.0})
+    node_0b = graph.add_node({DEFAULT_ATTR_KEYS.T: 0, "x": 0.5, "y": 0.0})
+
+    # At t=1: two nodes slightly further
+    node_1a = graph.add_node({DEFAULT_ATTR_KEYS.T: 1, "x": 1.0, "y": 0.0})
+    node_1b = graph.add_node({DEFAULT_ATTR_KEYS.T: 1, "x": 1.5, "y": 0.0})
+
+    # At t=2: one node at (2, 0)
+    node_2 = graph.add_node({DEFAULT_ATTR_KEYS.T: 2, "x": 2.0, "y": 0.0})
+
+    # With neighbors_per_frame=True, delta_t=2, n_neighbors=2:
+    # Node at t=2 should connect to 2 closest from t=0 AND 2 closest from t=1
+    # (4 edges total if all are within distance threshold)
+    operator = DistanceEdges(
+        distance_threshold=5.0,
+        n_neighbors=2,
+        delta_t=2,
+        neighbors_per_frame=True,
+    )
+
+    operator.add_edges(graph)
+
+    edges_df = graph.edge_attrs()
+    assert len(edges_df) == 4 + 2 + 2  # 4 edges from node_2 and 2 edges from node_1a, node_1b
+
+    t2_edges = edges_df.filter(edges_df[DEFAULT_ATTR_KEYS.EDGE_TARGET] == node_2)
+
+    # Should have 4 edges (2 from t=0, 2 from t=1)
+    assert len(t2_edges) == 4
+
+    # Should include nodes from both t=0 and t=1
+    sources = set(t2_edges[DEFAULT_ATTR_KEYS.EDGE_SOURCE].to_list())
+    assert sources == {node_0a, node_0b, node_1a, node_1b}
+
+
+def test_distance_edges_neighbors_per_frame_with_distance_threshold() -> None:
+    """Test neighbors_per_frame=True respects distance threshold per frame."""
+    graph = RustWorkXGraph()
+
+    # Register attribute keys
+    graph.add_node_attr_key("x", 0.0)
+    graph.add_node_attr_key("y", 0.0)
+
+    # Add nodes at t=0 (far away), t=1 (close), t=2
+    # At t=0: nodes very far from where t=2 node will be
+    graph.add_node({DEFAULT_ATTR_KEYS.T: 0, "x": 0.0, "y": 0.0})
+
+    # At t=1: node close to where t=2 node will be
+    node_1 = graph.add_node({DEFAULT_ATTR_KEYS.T: 1, "x": 2.0, "y": 0.0})
+
+    # At t=2: node at (2.5, 0)
+    node_2 = graph.add_node({DEFAULT_ATTR_KEYS.T: 2, "x": 2.5, "y": 0.0})
+
+    # With a tight distance threshold, only t=1 node should connect
+    operator = DistanceEdges(
+        distance_threshold=1.0,  # Only t=1 node is within 1.0 units
+        n_neighbors=2,
+        delta_t=2,
+        neighbors_per_frame=True,
+    )
+
+    operator.add_edges(graph)
+
+    edges_df = graph.edge_attrs()
+    t2_edges = edges_df.filter(edges_df[DEFAULT_ATTR_KEYS.EDGE_TARGET] == node_2)
+
+    # Should have only 1 edge from t=1 (t=0 is too far)
+    assert len(t2_edges) == 1
+    assert t2_edges[DEFAULT_ATTR_KEYS.EDGE_SOURCE][0] == node_1
+
+
+def test_distance_edges_neighbors_per_frame_single_delta_t() -> None:
+    """Test that neighbors_per_frame behaves same as False when delta_t=1."""
+    graph = RustWorkXGraph()
+
+    # Register attribute keys
+    graph.add_node_attr_key("x", 0.0)
+    graph.add_node_attr_key("y", 0.0)
+
+    # Add nodes at t=0 and t=1
+    for i in range(3):
+        graph.add_node({DEFAULT_ATTR_KEYS.T: 0, "x": float(i), "y": 0.0})
+
+    graph.add_node({DEFAULT_ATTR_KEYS.T: 1, "x": 1.0, "y": 0.0})
+
+    # Test with neighbors_per_frame=False
+    operator_false = DistanceEdges(
+        distance_threshold=5.0,
+        n_neighbors=2,
+        delta_t=1,
+        neighbors_per_frame=False,
+    )
+
+    graph_copy = graph.copy()
+    operator_false.add_edges(graph)
+
+    # Test with neighbors_per_frame=True
+    operator_true = DistanceEdges(
+        distance_threshold=5.0,
+        n_neighbors=2,
+        delta_t=1,
+        neighbors_per_frame=True,
+    )
+
+    operator_true.add_edges(graph_copy)
+
+    # Both should produce the same number of edges for delta_t=1
+    assert graph.num_edges() == graph_copy.num_edges()
